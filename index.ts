840771734360214e12badcfe6aebde8755feae57
Pierre Dubosq <pierre@dubosq.eu>
2023-03-16 11:25:17 -0600

    Feat: add datetime method

--- index.ts
+++ index.ts
@@ -1,4 +1,14 @@
 class Poseidon {
+  /**
+   * @brief Get the formatted date and time
+   * 
+   * @returns The formatted date and time
+   */
+  private static datetime(): string {
+    const date = new Date();
+
+    return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+  }
 }
 
 export default Poseidon;
